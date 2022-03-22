c08e5c682f69df1b4ae243d5d7427e0b3686506c
Mohammad Arshad <mdarshad1000@gmail.com>
2022-03-22 22:25:18 +0530

    tweaked app for heroku

---
+++ patient.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS patient(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(30) NOT NULL,
+                    birthday VARCHAR(30) NOT NULL,
+                    gender VARCHAR(15) NOT NULL,
+                    spot INT)
